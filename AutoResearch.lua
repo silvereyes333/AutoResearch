22c1dca3e4b6272926eba6bfe12a8297370e7db3
silvereyes <silvereyes333@gmail.com>
2017-02-17 08:32:15 -0600

    Version 1.2.0.0
    - Add /researchexport and /researchcraft commands to batch-craft all equipment that another character still needs for research

--- AutoResearch.lua
+++ AutoResearch.lua
@@ -1,7 +1,7 @@
 AutoResearch = {
     name = "AutoResearch",
     title = "Auto Research",
-    version = "1.1.0",
+    version = "1.2.0",
     author = "|c99CCEFsilvereyes|r",
 }
 local self = AutoResearch
@@ -26,7 +26,8 @@ local cheapStyles = {
     [ITEMSTYLE_AREA_REACH]        = true,
     [ITEMSTYLE_ENEMY_PRIMITIVE]   = true,
 }
-local function DiscoverResearchableTraits(craftSkill, researchLineIndex)
+ZO_CreateStringId("SI_AUTORESEARCH_INSUFFICIENT_STYLE_MATERIALS", "Insufficent style materials")
+local function DiscoverResearchableTraits(craftSkill, researchLineIndex, returnAll)
     
     -- Get the total number of traits in the research line
     local _, _, numTraits = GetSmithingResearchLineInfo(craftSkill, researchLineIndex)
@@ -44,14 +45,15 @@ local function DiscoverResearchableTraits(craftSkill, researchLineIndex)
         if not known then  
             local durationSecs = GetSmithingResearchLineTraitTimes(craftSkill, researchLineIndex, traitIndex)
             
-            if durationSecs then
-                -- No additional research can be done in this line right now.
-                self.researchableTraits[researchLineIndex] = nil
-                self.currentResearchCount = self.currentResearchCount + 1
-                return true
-            else
+            if not durationSecs then
                 -- Trait is researchable
                 table.insert(self.researchableTraits[researchLineIndex], traitIndex)
+                
+            elseif not returnAll then
+                -- No additional research can be done in this line right now.
+                self.currentResearchCount = self.currentResearchCount + 1
+                self.researchableTraits[researchLineIndex] = nil
+                return true
             end
             
         end
@@ -74,7 +76,7 @@ local function IsItemLocked(bagId, slotIndex)
         return true
     end
 end]]--
-local function GetResearchableItem(inventoryType, craftSkill, researchLineIndex)
+local function GetResearchableItem(inventoryType, craftSkill, researchLineIndex, returnAll)
     local inventory = PLAYER_INVENTORY.inventories[inventoryType]
     local bagId = inventory.backingBag
 
@@ -95,7 +97,7 @@ local function GetResearchableItem(inventoryType, craftSkill, researchLineIndex)
                 if not researchableItems[traitIndex] 
                    and CanItemBeSmithingTraitResearched(bagId, slotIndex, craftSkill, researchLineIndex, traitIndex) 
                 then
-                    if i == 1 then
+                    if i == 1 and not returnAll then
                         return slotIndex
                     end
                     researchableItems[traitIndex] = slotIndex
@@ -109,14 +111,20 @@ local function GetResearchableItem(inventoryType, craftSkill, researchLineIndex)
 end
 local function ResearchItem(bagId, slotIndex)
     local itemLink = GetItemLink(bagId, slotIndex)
-    
     local traitType = GetItemLinkTraitInfo(itemLink)
     local traitName = GetString("SI_ITEMTRAITTYPE", traitType)
     d("Researching "..tostring(itemLink).." ("..tostring(traitName)..")")
+    self.researching = true
     ResearchSmithingTrait(bagId, slotIndex)
+end
+local function StopResearching()
+    self.researching = nil
 end
 local function End()
     EVENT_MANAGER:UnregisterForEvent(self.name, EVENT_SMITHING_TRAIT_RESEARCH_STARTED)
+    EVENT_MANAGER:UnregisterForEvent(self.name, EVENT_CRAFT_COMPLETED)
+    -- Small delay to prevent last extraction failed message
+    zo_callLater(StopResearching, 500)
 end
 local function TryWritCreator(craftSkill)
 	End()
@@ -230,6 +238,259 @@ local function Start(eventCode, craftSkill, sameStation)
                                    OnSmithingTraitResearchStarted)
     ResearchNext(craftSkill)
 end
+
+local function OnAlertNoSuppression(category, soundId, message)
+    if not self.researching or category ~= UI_ALERT_CATEGORY_ALERT then
+        return
+    end
+    if message == SI_SMITHING_BLACKSMITH_EXTRACTION_FAILED 
+       or message == SI_SMITHING_CLOTHIER_EXTRACTION_FAILED
+       or message == SI_SMITHING_WOODWORKING_EXTRACTION_FAILED
+    then
+        return true
+    end
+end
+-- Stolen from pChat. Thanks Ayantir.
+-- Set copied text into text entry, if possible
+local function CopyToTextEntry(message)
+
+    -- Max of inputbox is 351 chars
+    if string.len(message) < 351 then
+        if CHAT_SYSTEM.textEntry:GetText() == "" then
+            CHAT_SYSTEM.textEntry:Open(message)
+            ZO_ChatWindowTextEntryEditBox:SelectAll()
+        end
+    end
+
+end
+local function EndCraft()
+    EVENT_MANAGER:UnregisterForEvent(self.name, EVENT_CRAFT_COMPLETED)
+    self.craftGear = nil
+    self.maxCraftCount = nil
+end
+local function MarkTraitCrafted(patternIndex, traitIndex)
+    self.craftGear[patternIndex][traitIndex] = nil
+    self.maxCraftCount = self.maxCraftCount - 1
+    if not next(self.craftGear[patternIndex]) then
+        --d("clearing out traits for research line index "..tostring(patternIndex))
+        self.craftGear[patternIndex] = nil
+    end
+end
+local function CraftNext()
+
+    if self.maxCraftCount < 1 then
+        EndCraft()
+        return
+    end
+    
+    -- check inventory for available slot
+    local slotIndex = FindFirstEmptySlotInBag(BAG_BACKPACK)
+    if not slotIndex then
+        ZO_AlertEvent(EVENT_INVENTORY_IS_FULL, 1, 0)
+        EndCraft()
+        return
+    end
+    
+    -- Check inventory for sufficient materials
+    local patternIndex = next(self.craftGear)
+    if not patternIndex then
+        EndCraft()
+        return
+    end
+    --d("research line index: "..tostring(patternIndex))
+    local materialIndex = 1 -- always use the cheap stuff
+    local materialCount = GetCurrentSmithingMaterialItemCount(patternIndex, materialIndex)
+    local materialRequired = GetSmithingPatternNextMaterialQuantity(patternIndex, 
+                                                                    materialIndex, 1, 1)
+    if materialCount < materialRequired then
+        EndCraft()
+        return
+    end
+    
+    -- Check inventory for trait stone
+    local traitItemIndex = next(self.craftGear[patternIndex])
+    --d("trait item index: "..tostring(traitItemIndex))
+    local craftSkill = GetCraftingInteractionType()
+    local traitStoneCount = GetCurrentSmithingTraitItemCount(traitItemIndex)
+    if traitStoneCount == 0 then
+        d("no trait stones. patternIndex:"..tostring(patternIndex)..",traitIndex:"..tostring(traitItemIndex))
+        MarkTraitCrafted(patternIndex, traitItemIndex)
+        CraftNext()
+        return
+    end
+    
+    -- find style stone with the biggest stack
+    local maxStyleItemStackSize = 0
+    local selectedStyleItemIndex
+    local selectedItemStyle
+    local maxStyleItemIndex = GetNumSmithingStyleItems()
+    for styleItemIndex = 1, maxStyleItemIndex do
+        local _, _, _, _, itemStyle = GetSmithingStyleItemInfo(styleItemIndex)
+        local styleItemStackSize = GetCurrentSmithingStyleItemCount(styleItemIndex)
+        if IsSmithingStyleKnown(styleItemIndex, patternIndex)
+           and cheapStyles[itemStyle]
+           and styleItemStackSize > maxStyleItemStackSize
+        then
+            selectedStyleItemIndex = styleItemIndex
+            maxStyleItemStackSize = styleItemStackSize  
+            selectedItemStyle = itemStyle       
+        end
+    end
+    
+    -- No cheap style materials found for any known styles
+    if not selectedStyleItemIndex then
+        ZO_Alert(UI_ALERT_CATEGORY_ERROR, SOUNDS.NEGATIVE_CLICK, 
+                 SI_AUTORESEARCH_INSUFFICIENT_STYLE_MATERIALS)
+        EndCraft()
+        return
+    end
+    
+    local styleItemLink = GetSmithingStyleItemLink(selectedStyleItemIndex)
+    local materialLink = GetSmithingPatternMaterialItemLink(patternIndex, materialIndex)
+    local traitItemLink = GetSmithingTraitItemLink(traitItemIndex)
+    local itemLink = GetSmithingPatternResultLink(patternIndex, materialIndex, materialRequired, selectedStyleItemIndex, traitItemIndex)
+    --local itemStyleName = zo_strformat("<<1>>", GetString("SI_ITEMSTYLE", selectedItemStyle))
+    local traitName = GetString("SI_ITEMTRAITTYPE", GetSmithingTraitItemInfo(traitItemIndex))
+    d("Crafting "..itemLink.." ("..traitName..") using "..tostring(materialRequired).."x "..materialLink..", "..styleItemLink.." and "..traitItemLink.."...")
+    -- Craft the item, at last
+    MarkTraitCrafted(patternIndex, traitItemIndex)
+    --CraftNext()
+    CraftSmithingItem(patternIndex, materialIndex, materialRequired, 
+                      selectedStyleItemIndex, traitItemIndex)
+end
+local function ResearchCraft(encoded)
+    if not encoded then
+        d("Expected encoded research trait list. Please run /researchexport on the toon you want to craft for, and then copy/paste the resulting command here.")
+        return
+    end
+    
+    local craftSkill = GetCraftingInteractionType()
+    if not researchableCraftSkills[craftSkill] then
+        d("You cannot craft researchable gear here. Please go to an equipment crafting station and try again.")
+        return
+    end
+    
+    local isLine = true
+    local isCraftSkill = true
+    local isFreeSlots = false
+    self.craftGear = {}
+    local substitutions = { ["Robe"] = "Robe & Jerkin", ["Jerkin"] = "Robe & Jerkin"}
+    
+    self.traitTypeToIndexMap = {}
+    for traitItemIndex = 1, GetNumSmithingTraitItems() do
+        local itemTraitType = GetSmithingTraitItemInfo(traitItemIndex)
+        if itemTraitType then
+            self.traitTypeToIndexMap[itemTraitType] = traitItemIndex
+        end
+    end
+    local nameToPatternMap = {}
+    for patternIndex = 1, GetNumSmithingPatterns() do
+        local name = GetSmithingPatternInfo(patternIndex)
+        if substitutions[name] then
+            name = substitutions[name]
+        end
+        nameToPatternMap[name] = patternIndex
+    end
+    local patternIndex
+    local researchLineIndex
+    for part in string.gmatch(encoded, '([^:]+)') do
+        if isCraftSkill then
+            isCraftSkill = false
+            if tonumber(part) ~= craftSkill then
+                d("You cannot craft that type of gear here. Please visit the appropriate craft station.")
+                return
+            end
+            isFreeSlots = true
+        elseif isFreeSlots then
+            self.maxCraftCount = tonumber(part)
+            isFreeSlots = false
+        elseif isLine then
+            researchLineIndex = tonumber(part)
+            local researchLineName = GetSmithingResearchLineInfo(craftSkill, researchLineIndex)
+            patternIndex = nameToPatternMap[researchLineName]
+            self.craftGear[patternIndex] = {}
+        else
+            for splitPart in string.gmatch(part, '([^,]+)') do
+                local researchTraitIndex = tonumber(splitPart)
+                local itemTraitType, _, known = GetSmithingResearchLineTraitInfo(craftSkill, researchLineIndex, researchTraitIndex)
+        
+                -- Trait is known
+                if known then  
+                    local traitItemIndex = self.traitTypeToIndexMap[itemTraitType]
+                    self.craftGear[patternIndex][traitItemIndex] = true
+                end
+            end
+        end
+        isLine = not isLine
+    end
+    
+    EVENT_MANAGER:RegisterForEvent(self.name, EVENT_CRAFT_COMPLETED, CraftNext)
+    CraftNext()
+end
+local function ResearchExport(skill)
+    
+    local craftSkill
+    if skill == "smith" or skill == "bs" or skill == "blacksmithing" or skill == "metal" then
+        craftSkill = CRAFTING_TYPE_BLACKSMITHING
+    elseif skill == "cloth" or skill == "clothier" then
+        craftSkill = CRAFTING_TYPE_CLOTHIER
+    elseif skill == "ww" or skill == "woodworking" then
+        craftSkill = CRAFTING_TYPE_WOODWORKING
+    else
+        d("Unrecognized skill line. Expected blacksmithing (bs, metal, smith), clothier (cloth) or woodworking (ww).")
+        return
+    end
+    
+    local freeSlots = GetNumBagFreeSlots(BAG_BACKPACK) - 20
+    if freeSlots < 0 then 
+        d("You do not have enough free slots in your inventory.")
+        return
+    end
+    local encoded = "/researchcraft "..tostring(craftSkill)..":"..tostring(freeSlots)..":"
+    
+    -- Subtotals how many traits are researched for each research line in this craft skill
+    self.researchableTraits = {}
+    
+    -- Total number of research lines for this craft skill
+    local researchLineCount = GetNumSmithingResearchLines(craftSkill)
+    
+    -- Loop through each research line (e.g. axe, mace, etc.)
+    local firstLine = true
+    for researchLineIndex = 1, researchLineCount do
+        -- Calculate subtotals for the research line
+        DiscoverResearchableTraits(craftSkill, researchLineIndex, true)
+        if self.researchableTraits[researchLineIndex] then
+            local backpackResearchables = GetResearchableItem(INVENTORY_BACKPACK, craftSkill, 
+                                                              researchLineIndex, true)
+            local bankResearchables     = GetResearchableItem(INVENTORY_BANK, craftSkill, 
+                                                              researchLineIndex, true)
+            local firstTrait = true
+            for i = 1, #self.researchableTraits[researchLineIndex] do
+                local traitIndex = self.researchableTraits[researchLineIndex][i]
+                local traitType = GetSmithingResearchLineTraitInfo(craftSkill, researchLineIndex, 
+                                                                   traitIndex)
+                if not backpackResearchables[traitIndex] and not bankResearchables[traitIndex] 
+                   and traitType ~= ITEM_TRAIT_TYPE_ARMOR_NIRNHONED
+                   and traitType ~= ITEM_TRAIT_TYPE_WEAPON_NIRNHONED
+                then
+                    if firstTrait then
+                        if firstLine then
+                            firstLine = false
+                        else
+                            encoded = encoded .. ":"
+                        end
+                        encoded = encoded .. tostring(researchLineIndex) .. ":"
+                        firstTrait = false
+                    else
+                        encoded = encoded .. ","
+                    end
+                    encoded = encoded .. tostring(traitIndex)
+                end
+            end
+        end
+    end 
+    CopyToTextEntry(encoded)
+end
 local function OnAddonLoaded(event, name)
     if name ~= self.name then return end
     EVENT_MANAGER:UnregisterForEvent(self.name, EVENT_ADD_ON_LOADED)
@@ -241,5 +502,11 @@ local function OnAddonLoaded(event, name)
     end
     EVENT_MANAGER:RegisterForEvent(self.name, EVENT_CRAFTING_STATION_INTERACT, Start)
     EVENT_MANAGER:RegisterForEvent(self.name, EVENT_END_CRAFTING_STATION_INTERACT, End)
+    
+    ZO_PreHook("ZO_AlertNoSuppression", OnAlertNoSuppression)
+    SLASH_COMMANDS["/rexport"] = ResearchExport
+    SLASH_COMMANDS["/researchexport"] = ResearchExport
+    SLASH_COMMANDS["/rcraft"] = ResearchCraft
+    SLASH_COMMANDS["/researchcraft"] = ResearchCraft
 end
 EVENT_MANAGER:RegisterForEvent(self.name, EVENT_ADD_ON_LOADED, OnAddonLoaded)
