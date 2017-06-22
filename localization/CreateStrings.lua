44a8571d3a4eb467fb68895700b806a6a8964336
silvereyes <silvereyes333@gmail.com>
2017-06-22 01:01:02 -0500

    Version 1.6.0
    - Selects research line (e.g. axe, cuirass, girdle, etc) based on both research times and custom trait order instead of left to right
    - Now selects items from the bank before the backpack *if* they have lower research times and/or custom trait orders
    - Fixed bug causing client hang/crash when using Auto Research, pChat and either Preposterous or Colorful ESO
    - Fixed issue with Dolgubon's Lazy Writ Crafter not starting for characters experiencing the base game bug with research slot #3 not working

--- localization/CreateStrings.lua
+++ localization/CreateStrings.lua
@@ -2,3 +2,6 @@ for stringId, value in pairs(AUTORESEARCH_STRINGS) do
     ZO_CreateStringId(stringId, value)
 end
 AUTORESEARCH_STRINGS = nil
+ZO_CreateStringId("SI_AUTORESEARCH_ERROR", 
+                  "|cFF0000"..GetString(SI_ITEM_ACTION_RESEARCH)
+                  .." "..GetString(SI_PROMPT_TITLE_ERROR).."|r")
