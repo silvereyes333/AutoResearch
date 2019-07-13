471ef8f9738e1b09c199359ce502b52ef0400ec7
Baertram <pz1@gmx.net>
2019-07-13 19:42:26 +0200

    Changed german translations
    Changed german translations

--- localization/DE.lua
+++ localization/DE.lua
@@ -1,12 +1,12 @@
 local strings = {
-    ["SI_AUTORESEARCH_BAGS"]                   = "Taschen für die Suche nach Ausrüstung für die Forschung",
+    ["SI_AUTORESEARCH_BAGS"]                   = "Ausrüstungs-Taschen durchsuchen für Analyse",
     ["SI_AUTORESEARCH_STYLES"]                 = "Stile zum Analysieren",
     ["SI_AUTORESEARCH_SETS"]                   = "Set-Gegenstände zum Analysieren",
     ["SI_AUTORESEARCH_CHAT_MESSAGES"]          = "Chat-Nachrichten",
     ["SI_AUTORESEARCH_SHORT_PREFIX"]           = "Kurzes Präfix für Nachrichten",
-    ["SI_AUTORESEARCH_SHORT_PREFIX_TOOLTIP"]   = "Wenn aktiviert, wird bei alle Chat-Nachrichten AR anstelle von AutoResearch vorangestellt.",
-    ["SI_AUTORESEARCH_COLORED_PREFIX"]         = "Verwenden die AutoResearch 1 Präfix Farbe",
-    ["SI_AUTORESEARCH_COLORED_PREFIX_TOOLTIP"] = "Bewirkt, dass das Präfix für Chat-Nachrichten die blauen Farben von AutoResearch 1 (d. H. |c99CCEFAutoResearch|r oder |c99CCEFAR|r) anstelle der Einstellung für die Chat-Nachrichten Farbe verwendet.",
+    ["SI_AUTORESEARCH_SHORT_PREFIX_TOOLTIP"]   = "Wenn aktiviert, wird bei allen Chat-Nachrichten AR anstelle von AutoResearch vorangestellt.",
+    ["SI_AUTORESEARCH_COLORED_PREFIX"]         = "Verwende die AutoResearch 1 Präfix Farbe",
+    ["SI_AUTORESEARCH_COLORED_PREFIX_TOOLTIP"] = "Bewirkt, dass das Präfix für Chat-Nachrichten die blaue Farben von AutoResearch 1 (d. H. |c99CCEFAutoResearch|r oder |c99CCEFAR|r) anstelle der Einstellung für die Chat-Nachrichten Farbe verwendet.",
     ["SI_AUTORESEARCH_CHAT_USE_SYSTEM_COLOR"]  = "Dieselbe Farbe wie Systemnachrichten verwenden.",
     ["SI_AUTORESEARCH_CHAT_COLOR"]             = "Chat-Nachrichten Farbe",
 }
