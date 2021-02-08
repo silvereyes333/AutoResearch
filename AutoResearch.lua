404b6994c1dd5dbd3a7ae3cb82bb1366978bc968
silvereyes <silvereyes333@gmail.com>
2021-02-07 18:54:48 -0600

    Version 2.4.2
    - Update 29 Flames of Ambition compatibility
    - This version is compatible with both live (Markarth) and PTS (Flames of Ambition)

--- AutoResearch.lua
+++ AutoResearch.lua
@@ -11,7 +11,7 @@ AUTORESEARCH_ENABLE_SELECTED = 2
 AutoResearch = {
     name = "AutoResearch",
     title = "Auto Research",
-    version = "2.4.1",
+    version = "2.4.2",
     author = "silvereyes",
     
     -- Global details about armor, weapon TraitType value ranges.
